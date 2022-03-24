291bda98a52aecacbb7b8ef12d2c3a798dfe8c01
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-03-24 11:39:24 +0100

    feat: add endpoint for users to create accounts

---
+++ api/src/main/java/ch/pledarigrond/api/controllers/user/PublicUsersController.java
@@ -0,0 +1,36 @@
+package ch.pledarigrond.api.controllers.user;
+
+import ch.pledarigrond.api.services.UserService;
+import ch.pledarigrond.common.data.common.LightUserInfo;
+import ch.pledarigrond.mongodb.exceptions.InvalidUserException;
+import ch.pledarigrond.mongodb.model.PgUserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("user/users")
+public class PublicUsersController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping()
+    ResponseEntity<?> create(@Validated @RequestBody LightUserInfo user) {
+        PgUserInfo pgUserInfo = new PgUserInfo(user.getEmail());
+        pgUserInfo.setPassword(user.getPassword());
+
+        try {
+            return ResponseEntity.ok(userService.insert(pgUserInfo).toLightUser());
+        } catch (InvalidUserException e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exists");
+        }
+    }
+}
