49b97f06bb5025b0e19ab06339d883a549d2c3f2
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-04-18 17:22:45 +0200

    test: adding tests

---
+++ lucene/src/test/java/ch/pledarigrond/lucene/core/TestDefaultQueryBuilder.java
@@ -0,0 +1,158 @@
+package ch.pledarigrond.lucene.core;
+
+
+import ch.pledarigrond.common.config.LuceneConfiguration;
+import ch.pledarigrond.common.data.common.*;
+import ch.pledarigrond.common.data.user.Pagination;
+import ch.pledarigrond.common.data.user.SearchCriteria;
+import ch.pledarigrond.lucene.exceptions.BrokenIndexException;
+import ch.pledarigrond.lucene.exceptions.InvalidQueryException;
+import ch.pledarigrond.lucene.exceptions.NoIndexAvailableException;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.data.domain.Page;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class TestDefaultQueryBuilder {
+	
+	private LuceneIndex luceneIndex;
+
+	private File indexDir;
+
+	@Before
+	public void beforeTest() throws Exception {
+		Language language = Language.SURMIRAN;
+		File file = File.createTempFile("pledarigrond", "test");
+		indexDir = new File(file.getParentFile(), "pg_test" + UUID.randomUUID().toString() + "_idx");
+		Assert.assertFalse(indexDir.exists());
+		indexDir.mkdir();
+		file.deleteOnExit();
+		LuceneConfiguration luceneConfiguration = new LuceneConfiguration(language, indexDir.getAbsolutePath());
+		luceneIndex = new LuceneIndex(luceneConfiguration);
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("dictionary.tsv");
+		BufferedReader br = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+		String line = null;
+		List<LexEntry> entries = new ArrayList<LexEntry>();
+		int counter = 0;
+		while((line = br.readLine()) != null) {
+			String[] parts = line.split("\t");
+			LemmaVersion lv = new LemmaVersion();
+			lv.setValue("DStichwort", parts[0]);
+			lv.setValue("RStichwort", parts[1]);
+			lv.setVerification(LemmaVersion.Verification.ACCEPTED);
+			lv.setStatus(LemmaVersion.Status.NEW_ENTRY);
+			LexEntry entry = new LexEntry(lv);
+			entry.setId(counter+"");
+			counter++;
+			entries.add(entry);
+		}
+		luceneIndex.addToIndex(entries.iterator());
+	}
+	
+	@After
+	public void afterTest() throws Exception {
+		deleteRecursive(indexDir);
+	}
+	
+	@Test
+	public void testPrefixQuery() throws InvalidQueryException, NoIndexAvailableException, BrokenIndexException, IOException, InvalidTokenOffsetsException {
+		Page<LemmaVersion> result = luceneIndex.query(getSearchCriteria(SearchDirection.GERMAN, SearchMethod.PREFIX, "haus"), getPagination());
+		Set<String> found = getStrings("DStichwort", result);
+		String[] expected = new String[] {"hausen", "Haushalt", "Haus"};
+		validateResult(found, expected);
+	}
+	
+	@Test
+	public void testInfixQuery() throws InvalidQueryException, NoIndexAvailableException, BrokenIndexException, IOException, InvalidTokenOffsetsException {
+		Page<LemmaVersion> result = luceneIndex.query(getSearchCriteria(SearchDirection.GERMAN, SearchMethod.INTERN, "haus"), getPagination());
+		Set<String> found = getStrings("DStichwort", result);
+		String[] expected = new String[] {"nach Hause", "Das Haus brennt", "Flohausstellung", "hausen", "Haushalt", "zu Hause", "Wohnhaus", "Haus", "Kranken(haus)"};
+		validateResult(found, expected);
+	}
+	
+	@Test
+	public void testSuffix() throws InvalidQueryException, NoIndexAvailableException, BrokenIndexException, IOException, InvalidTokenOffsetsException {
+		Page<LemmaVersion> result = luceneIndex.query(getSearchCriteria(SearchDirection.GERMAN, SearchMethod.SUFFIX, "haus"), getPagination());
+		Set<String> found = getStrings("DStichwort", result);
+		String[] expected = new String[] {"Wohnhaus", "Haus", "Kranken(haus)"};
+		validateResult(found, expected);
+	}
+	
+	@Test
+	public void testExact() throws InvalidQueryException, NoIndexAvailableException, BrokenIndexException, IOException, InvalidTokenOffsetsException {
+		Page<LemmaVersion> result = luceneIndex.query(getSearchCriteria(SearchDirection.GERMAN, SearchMethod.EXACT, "haus"), getPagination());
+		Set<String> found = getStrings("DStichwort", result);
+		String[] expected = new String[] {"Haus"};
+		validateResult(found, expected);
+	}
+	
+	@Test
+	public void testDefault() throws InvalidQueryException, NoIndexAvailableException, BrokenIndexException, IOException, InvalidTokenOffsetsException {
+		Page<LemmaVersion> result = luceneIndex.query(getSearchCriteria(SearchDirection.GERMAN, SearchMethod.NORMAL, "haus"), getPagination());
+		Set<String> found = getStrings("DStichwort", result);
+		String[] expected = new String[] {"Das Haus brennt", "hausen", "Haushalt", "Haus", "Kranken(haus)"};
+		validateResult(found, expected);
+	}
+	
+	@Test
+	public void testAlphabeticalIndex() throws NoIndexAvailableException, BrokenIndexException, InvalidQueryException {
+		Page<LemmaVersion> result = luceneIndex.getAllStartingWith(SearchDirection.GERMAN, "h", 0);
+		Set<String> found = getStrings("DStichwort", result);
+		String[] expected = new String[] {"heimwärts", "hausen", "Haushalt"};
+		validateResult(found, expected);
+	}
+	
+	@Test
+	public void testExactQuery() throws NoIndexAvailableException, BrokenIndexException, InvalidQueryException {
+		Page<LemmaVersion> result = luceneIndex.queryExact("haus", DictionaryLanguage.GERMAN);
+		Set<String> found = getStrings("DStichwort", result);
+		String[] expected = new String[] {"Haus"};
+		validateResult(found, expected);
+	}
+	
+	
+	private Set<String> getStrings(String key, Page<LemmaVersion> result) {
+		HashSet<String> toReturn = new HashSet<String>();
+		List<LemmaVersion> entries = result.getContent();
+		for (LemmaVersion lv : entries) {
+			toReturn.add(lv.getEntryValue(key));
+		}
+		return toReturn;
+	}
+
+	private SearchCriteria getSearchCriteria(SearchDirection direction, SearchMethod method, String searchPhrase) {
+		SearchCriteria query = new SearchCriteria();
+		query.setSearchDirection(direction);
+		query.setSearchMethod(method);
+		query.setSearchPhrase(searchPhrase);
+		return query;
+	}
+
+	private Pagination getPagination() {
+		return new Pagination();
+	}
+
+	private void validateResult(Set<String> found, String[] expected) {
+		List<String> expectedList = Arrays.asList(expected);
+		Assert.assertTrue("Was expecting to find " + expectedList + " in " + found, found.containsAll(expectedList));
+		found.removeAll(expectedList);
+		Assert.assertTrue("Found more items than expecting: " + found, found.isEmpty());
+	}
+
+	private void deleteRecursive(File fileOrDir) {
+		if(fileOrDir.isDirectory()) {
+			File[] files = fileOrDir.listFiles();
+			for (File file : files) {
+				deleteRecursive(file);
+			}
+		} else {
+			fileOrDir.delete();
+		}
+	}
+}
---
+++ lucene/src/test/java/ch/pledarigrond/lucene/core/TestIndexCRUD.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Copyright 2013 Sprachliche Informationsverarbeitung, University of Cologne
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package ch.pledarigrond.lucene.core;
+
+import ch.pledarigrond.common.config.LuceneConfiguration;
+import ch.pledarigrond.common.data.common.*;
+import ch.pledarigrond.common.data.lucene.IndexStatistics;
+import org.junit.*;
+import org.springframework.data.domain.Page;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public class TestIndexCRUD {
+
+	private LuceneIndex luceneIndex;
+
+	private File indexDir;
+	
+	@Before
+	public void beforeTest() throws Exception {
+		Language language = Language.SURMIRAN;
+		File file = File.createTempFile("pledarigrond", "test");
+		indexDir = new File(file.getParentFile(), "pg_test" + UUID.randomUUID().toString() + "_idx");
+		Assert.assertFalse(indexDir.exists());
+		indexDir.mkdir();
+		file.deleteOnExit();
+		LuceneConfiguration luceneConfiguration = new LuceneConfiguration(language, indexDir.getAbsolutePath());
+		luceneIndex = new LuceneIndex(luceneConfiguration);
+	}
+	
+	@After
+	public void afterTest() throws Exception {
+		deleteRecursive(indexDir);
+	}
+	
+	private void deleteRecursive(File fileOrDir) {
+		if(fileOrDir.isDirectory()) {
+			File[] files = fileOrDir.listFiles();
+			for (File file : files) {
+				deleteRecursive(file);
+			}
+		} else {
+			fileOrDir.delete();
+		}
+	}
+
+	private LexEntry generateValidEntry() {
+		LemmaVersion lv = new LemmaVersion();
+		lv.putEntryValue("DStichwort", "a" + UUID.randomUUID());
+		lv.putEntryValue("RStichwort", "b" + UUID.randomUUID());
+		LexEntry entry = new LexEntry(lv);
+		lv.setVerification(LemmaVersion.Verification.ACCEPTED);
+		entry.setId(UUID.randomUUID().toString());
+		return entry;
+	}
+
+	@Ignore
+	@Test
+	public void testGetStatistics() {
+		IndexStatistics stats = luceneIndex.getIndexStatistics();
+		Assert.assertNotNull(stats);
+	}
+	
+	@Test
+	public void testDropIndex() throws Exception  {
+		luceneIndex.dropIndex();
+		luceneIndex.reloadIndex();
+		IndexStatistics statistics = luceneIndex.getIndexStatistics();
+		Assert.assertEquals(0, statistics.getNumberOfEntries());
+	}
+	
+	
+	@Test
+	public void testCreateIndex() throws Exception  {
+		testDropIndex();
+		List<LexEntry> entries = new ArrayList<LexEntry>();
+		for(int i = 0;  i < 5; i++) {
+			entries.add(generateValidEntry());
+		}
+		luceneIndex.addToIndex(entries.iterator());
+		luceneIndex.reloadIndex();
+		IndexStatistics statistics = luceneIndex.getIndexStatistics();
+		Assert.assertEquals(entries.size(),statistics.getNumberOfEntries());
+		Page<LemmaVersion> results = luceneIndex.getAllStartingWith(SearchDirection.GERMAN, "a", 0);
+		Assert.assertTrue(results.getContent().size() > 0);
+		results = luceneIndex.getAllStartingWith(SearchDirection.ROMANSH, "x", 0);
+		Assert.assertEquals(0, results.getContent().size());
+	}
+	
+	@Test
+	public void testUpdateIndex() throws Exception  {
+		testCreateIndex();
+		IndexStatistics beforeUpdate = luceneIndex.getIndexStatistics();
+		List<LexEntry> entries = new ArrayList<LexEntry>();
+		for(int i = 0;  i < 5; i++) {
+			entries.add(generateValidEntry());
+		}
+		luceneIndex.addToIndex(entries.iterator());
+		luceneIndex.reloadIndex();
+		IndexStatistics afterUpdate = luceneIndex.getIndexStatistics();
+		Assert.assertEquals(afterUpdate.getNumberOfEntries(), beforeUpdate.getNumberOfEntries() * 2);
+	}
+}
