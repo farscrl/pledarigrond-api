9eddde353b50d0f5460bb9e00a584e656dd355d8
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-05-04 12:53:05 +0200

    test: add tests for uppercase strings

--- inflection/src/test/java/ch/pledarigrond/inflection/generation/surmiran/SurmiranNounGeneratorTest.java
+++ inflection/src/test/java/ch/pledarigrond/inflection/generation/surmiran/SurmiranNounGeneratorTest.java
@@ -19,13 +19,13 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testPrintOutForms() throws IOException {
-        forms = generator.generateForms("scolast", "3");
+        forms = generator.generateForms("3", "scolast");
         generator.printForms(forms.getInflectionValues(), "test");
     }
 
     @Test
     public void testRule3() {
-        forms = generator.generateForms("scolast", "3");
+        forms = generator.generateForms("3", "scolast");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "scolast");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "scolasta");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "scolasts");
@@ -34,7 +34,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule4() {
-        forms = generator.generateForms("manader", "4");
+        forms = generator.generateForms("4", "manader");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "manader");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "manadra");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "manaders");
@@ -43,7 +43,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule5() {
-        forms = generator.generateForms("responsabel", "5");
+        forms = generator.generateForms("5", "responsabel");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "responsabel");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "responsabla");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "responsabels");
@@ -52,7 +52,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule6() {
-        forms = generator.generateForms("apotecher", "6");
+        forms = generator.generateForms("6", "apotecher");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "apotecher");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "apotecra");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "apotechers");
@@ -61,7 +61,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule7() {
-        forms = generator.generateForms("flevel", "7");
+        forms = generator.generateForms("7", "flevel");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "flevel");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "flevla");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "flevels");
@@ -70,7 +70,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule8() {
-        forms = generator.generateForms("gioven", "8");
+        forms = generator.generateForms("8", "gioven");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "gioven");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "giovna");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "giovens");
@@ -79,7 +79,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule9() {
-        forms = generator.generateForms("ampluia", "9");
+        forms = generator.generateForms("9", "ampluia");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "ampluia");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "amplueida");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "ampluias");
@@ -88,7 +88,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule10() {
-        forms = generator.generateForms("anvido", "10");
+        forms = generator.generateForms("10", "anvido");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "anvido");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "anvidada");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "anvidos");
@@ -97,7 +97,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule11() {
-        forms = generator.generateForms("relaschea", "11");
+        forms = generator.generateForms("11", "relaschea");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "relaschea");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "relascheda");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "relascheas");
@@ -106,7 +106,7 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testRule12() {
-        forms = generator.generateForms("minoren", "12");
+        forms = generator.generateForms("12", "minoren");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "minoren");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "minorenna");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "minorens");
@@ -115,29 +115,37 @@ public class SurmiranNounGeneratorTest {
 
     @Test
     public void testPluralRule2() {
-        forms = generator.generateForms("cass", "1");
+        forms = generator.generateForms("1", "cass");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "cass");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "cass");
     }
 
     @Test
     public void testPluralRule3() {
-        forms = generator.generateForms("isch-tgesa", "1");
+        forms = generator.generateForms("1", "isch-tgesa");
         Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "isch-tgesa");
         Assert.assertEquals(forms.getInflectionValues().get("mPlural"), "ischs-tgesa");
     }
 
     @Test
     public void testPluralRule4() {
-        forms = generator.generateForms("pale", "2");
+        forms = generator.generateForms("2", "pale");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "pale");
         Assert.assertEquals(forms.getInflectionValues().get("fPlural"), "paleis");
     }
 
     @Test
     public void testPluralRule5() {
-        forms = generator.generateForms("pare", "2");
+        forms = generator.generateForms("2", "pare");
         Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "pare");
         Assert.assertEquals(forms.getInflectionValues().get("fPlural"), "pareis");
+    }
+
+    @Test
+    public void testUpperCase() {
+        forms = generator.generateForms("3", "Argoves");
+        Assert.assertEquals(forms.getInflectionValues().get("mSingular"), "Argoves");
+        Assert.assertEquals(forms.getInflectionValues().get("fSingular"), "Argovesa");
+        Assert.assertEquals(forms.getInflectionValues().get("fPlural"), "Argovesas");
     }
 }
