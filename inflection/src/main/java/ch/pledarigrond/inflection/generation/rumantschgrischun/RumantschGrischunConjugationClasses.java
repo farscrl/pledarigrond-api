cefd38097be573736e4fd9dcfede9fe41c52f771
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-11-08 08:37:53 +0100

    fix: typography of RG conjugation class descriptions

--- inflection/src/main/java/ch/pledarigrond/inflection/generation/rumantschgrischun/RumantschGrischunConjugationClasses.java
+++ inflection/src/main/java/ch/pledarigrond/inflection/generation/rumantschgrischun/RumantschGrischunConjugationClasses.java
@@ -13,10 +13,10 @@ public class RumantschGrischunConjugationClasses {
                     new InflectionSubType("1", "1: -ar", "Finiziun sin -ar (chantar)"),
                     new InflectionSubType("2", "2. -air", "Finiziun sin -air (taschair)"),
                     new InflectionSubType("3", "3: -er", "Finiziun sin -er (vender)"),
-                    new InflectionSubType("4", "4: -ir", "Finiziun sin –ir  e sin –er cun 1./2.pl. sin –in-is (partir; assister – assistin, assistis, assistiva, assistiss, etc.)"),
-                    new InflectionSubType("5", "5: -ir (esch)", "Finiziun sin -ir cun infix esch (offrir)"),
-                    new InflectionSubType("6", "6: -ar (esch)", "Finiziun sin -ar cun infix esch (telefonar)"),
-                    new InflectionSubType("7", "7: 1. sing. sin -el", "1. sg. sin –el (liar – jau liel)")
+                    new InflectionSubType("4", "4: -ir", "Finiziun sin -ir e sin -er cun 1./2. pl. sin -in/-is (partir; assister – assistin, assistis, assistiva, assistiss, etc.)"),
+                    new InflectionSubType("5", "5: -ir (-esch-)", "Finiziun sin -ir cun infix -esch- (offrir)"),
+                    new InflectionSubType("6", "6: -ar (-esch-)", "Finiziun sin -ar cun infix -esch- (telefonar)"),
+                    new InflectionSubType("7", "7: 1. sg. sin -el", "(liar – jau liel)")
 
             )
     );
