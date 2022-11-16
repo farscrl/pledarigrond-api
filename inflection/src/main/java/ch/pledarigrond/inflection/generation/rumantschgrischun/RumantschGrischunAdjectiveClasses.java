9330e91381eb085ce0ed267ef4a7850c15e293a2
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-11-16 09:13:21 +0100

    feat: update examples for RG flexion lists

--- inflection/src/main/java/ch/pledarigrond/inflection/generation/rumantschgrischun/RumantschGrischunAdjectiveClasses.java
+++ inflection/src/main/java/ch/pledarigrond/inflection/generation/rumantschgrischun/RumantschGrischunAdjectiveClasses.java
@@ -10,14 +10,14 @@ public class RumantschGrischunAdjectiveClasses {
     private static final List<InflectionSubType> nounClasses = new ArrayList<>(
             Arrays.asList(
                     new InflectionSubType("1", "regla da basa","p.ex. puril -> purila, purils, purilas"),
-                    new InflectionSubType("2", "finiziun sen «der»","p.ex. veder -> vedra, veders, vedras"),
-                    new InflectionSubType("3", "finiziun sen «bel»","p.ex. admirabel -> admirabla, admirabels, admirablas"),
-                    new InflectionSubType("4", "finiziun sen «cher»","p.ex. mediocher – mediocra, mediochers, mediocras"),
-                    new InflectionSubType("5", "finiziun sen «vel»","p.ex. amiaivel – amiaivla, amiaivels, amiaivlas"),
-                    new InflectionSubType("6", "finiziun sen «ven»","p.ex. giuven – giuvna, giuvens, giuvnas"),
-                    new InflectionSubType("7", "finiziun sen «à»","p.ex. isolà – isolada, isolads, isoladas"),
-                    new InflectionSubType("8", "finiziun sen «ì»","p.ex. sclerì – sclerida, sclerids, scleridas"),
-                    new InflectionSubType("9", "finiziun sen «(vocal)(c/f/m/n/p/r/t/z)»","p.ex. tschuf – tschuffa, tschufs, tschuffas")
+                    new InflectionSubType("2", "finiziun sin «der»","p.ex. veder -> vedra, veders, vedras"),
+                    new InflectionSubType("3", "finiziun sin «bel»","p.ex. admirabel -> admirabla, admirabels, admirablas"),
+                    new InflectionSubType("4", "finiziun sin «cher»","p.ex. mediocher –> mediocra, mediochers, mediocras"),
+                    new InflectionSubType("5", "finiziun sin «vel»","p.ex. amiaivel –> amiaivla, amiaivels, amiaivlas"),
+                    new InflectionSubType("6", "finiziun sin «ven»","p.ex. giuven –> giuvna, giuvens, giuvnas"),
+                    new InflectionSubType("7", "finiziun sin «à»","p.ex. isolà –> isolada, isolads, isoladas"),
+                    new InflectionSubType("8", "finiziun sin «ì»","p.ex. sclerì –> sclerida, sclerids, scleridas"),
+                    new InflectionSubType("9", "finiziun sin «(vocal)(c/f/m/n/p/r/t/z)»","p.ex. tschuf –> tschuffa, tschufs, tschuffas")
             )
     );
 
--- inflection/src/main/java/ch/pledarigrond/inflection/generation/rumantschgrischun/RumantschGrischunNounClasses.java
+++ inflection/src/main/java/ch/pledarigrond/inflection/generation/rumantschgrischun/RumantschGrischunNounClasses.java
@@ -9,15 +9,15 @@ import java.util.List;
 public class RumantschGrischunNounClasses {
     private static final List<InflectionSubType> nounClasses = new ArrayList<>(
             Arrays.asList(
-                    new InflectionSubType("1", "mo masculin", "p.ex. il cass"),
-                    new InflectionSubType("2", "mo feminin", "p.ex. la pale"),
+                    new InflectionSubType("1", "mo masculin", "p.ex. il cas"),
+                    new InflectionSubType("2", "mo feminin", "p.ex. la palì"),
                     new InflectionSubType("3", "regular", "p.ex. scolast – scolasta"),
                     new InflectionSubType("4", "der -> dra", "p.ex. manader – manadra"),
-                    new InflectionSubType("5", "bel -> bla", "p.ex. responsabel -> responsabla"),
+                    new InflectionSubType("5", "bel -> bla", "p.ex. responsabel - responsabla"),
                     new InflectionSubType("6", "cher -> cra", "p.ex. apotecher – apotecra"),
                     new InflectionSubType("7", "à -> ada", "p.ex. deputà – deputada"),
                     new InflectionSubType("8", "(vocal))(c/f/m/n/p/r/t/z)", "p.ex. nar – narra"),
-                    new InflectionSubType("9", "plural coll", "p.ex. peir - peirs - peira")
+                    new InflectionSubType("9", "plural coll", "p.ex. pair - pairs - paira")
             )
     );
 
