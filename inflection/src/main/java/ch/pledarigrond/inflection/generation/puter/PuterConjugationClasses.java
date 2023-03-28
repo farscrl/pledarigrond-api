a92eb751a80f7b6984814643610576be971ac475
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-03-28 09:39:43 +0200

    fix: description in puter

--- inflection/src/main/java/ch/pledarigrond/inflection/generation/puter/PuterConjugationClasses.java
+++ inflection/src/main/java/ch/pledarigrond/inflection/generation/puter/PuterConjugationClasses.java
@@ -10,15 +10,15 @@ public class PuterConjugationClasses {
 
     private static final List<InflectionSubType> conjugationClasses = new ArrayList<>(
             Arrays.asList(
-                    new InflectionSubType("1", "-er", "Finiziun sin -er, accent sün la finiziun (p.ex. güder)"),
-                    new InflectionSubType("1a", "-er (-esch-)", "Finiziun sin -er, accent sün la finiziun, cun infix -esch- (p.ex. gratuler)", true),
+                    new InflectionSubType("1", "-er", "Finiziun sün -er, accent sün la finiziun (p.ex. güder)"),
+                    new InflectionSubType("1a", "-er (-esch-)", "Finiziun sün -er, accent sün la finiziun, cun infix -esch- (p.ex. gratuler)", true),
 
-                    new InflectionSubType("2", "-air", "Finiziun sin -air, accent sün finiziun (p.ex. giodair)"),
+                    new InflectionSubType("2", "-air", "Finiziun sün -air, accent sün finiziun (p.ex. giodair)"),
 
-                    new InflectionSubType("3", "-er", "Finiziun sin -er, accent sün tschep (p.ex. vender)"),
+                    new InflectionSubType("3", "-er", "Finiziun sün -er, accent sün tschep (p.ex. vender)"),
 
-                    new InflectionSubType("4", "-ir", "Finiziun sin -ir (p.ex. partir )"),
-                    new InflectionSubType("4a", "-ir", "Finiziun sin -ir, cun infix -esch- (p.ex. chapir)")
+                    new InflectionSubType("4", "-ir", "Finiziun sün -ir (p.ex. partir)"),
+                    new InflectionSubType("4a", "-ir", "Finiziun sün -ir, cun infix -esch- (p.ex. chapir)")
             )
     );
 
