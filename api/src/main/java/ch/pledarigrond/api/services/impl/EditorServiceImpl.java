b19d897ef4e8b21c9e8a1bbbf3f3085f46fab667
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-10-13 15:27:53 +0200

    feat: disable automatic changes filter for puter/vallader

--- api/src/main/java/ch/pledarigrond/api/services/impl/EditorServiceImpl.java
+++ api/src/main/java/ch/pledarigrond/api/services/impl/EditorServiceImpl.java
@@ -290,6 +290,6 @@ public class EditorServiceImpl implements EditorService {
     }
 
     private boolean excludeAutomaticChangesForLanguage(Language language) {
-        return language == Language.PUTER || language == Language.VALLADER;
+        return false;
     }
 }
