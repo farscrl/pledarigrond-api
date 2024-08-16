d6b946fb83e9cc94398221400b4292f9579b6b8b
Gion-Andri Cantieni <gion-andri@gmx.ch>
2024-08-16 14:10:42 +0200

    chore: clean up scheduled dump service

--- api/src/main/java/ch/pledarigrond/api/services/ScheduledDumpService.java
+++ api/src/main/java/ch/pledarigrond/api/services/ScheduledDumpService.java
@@ -14,17 +14,21 @@ import com.mongodb.DBObject;
 import com.mongodb.client.MongoCursor;
 import org.bson.Document;
 import org.json.JSONArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 @Service
 public class ScheduledDumpService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledDumpService.class);
 
     @Autowired
     private PgEnvironment pgEnvironment;
@@ -32,23 +36,19 @@ public class ScheduledDumpService {
     public File getJsonExportFile(Language language) {
         String dbName = DbSelector.getDbNameByLanguage(pgEnvironment, language);
         File dir = new File(pgEnvironment.getExportLocation() + dbName + "/");
-        dir.mkdirs();
+        boolean success = dir.mkdirs();
+        if (!success) {
+            return null;
+        }
 
-        if (dir.listFiles().length > 0) {
-            File file = dir.listFiles(new FileFilter() {
-                @Override
-                public boolean accept(File pathname) {
-                    return pathname.getName().endsWith(".zip");
-                }
-            })[0];
-
-            return file;
+        if (Objects.requireNonNull(dir.listFiles()).length > 0) {
+            return Objects.requireNonNull(dir.listFiles(pathname -> pathname.getName().endsWith(".zip")))[0];
         }
         return null;
     }
 
     @Scheduled(cron = "${pg.export.cron}")
-    private void exportJSON() throws IOException, NoDatabaseAvailableException, NoSuchAlgorithmException {
+    private void exportJSON() throws IOException, NoDatabaseAvailableException {
         exportDataForLanguage(Language.PUTER);
         exportDataForLanguage(Language.RUMANTSCHGRISCHUN);
         exportDataForLanguage(Language.SURMIRAN);
@@ -57,10 +57,14 @@ public class ScheduledDumpService {
         exportDataForLanguage(Language.VALLADER);
     }
 
-    private void exportDataForLanguage(Language language) throws IOException, NoDatabaseAvailableException, NoSuchAlgorithmException {
+    private void exportDataForLanguage(Language language) throws IOException, NoDatabaseAvailableException {
         String dbName = DbSelector.getDbNameByLanguage(pgEnvironment, language);
         File exportDir = new File(pgEnvironment.getExportLocation() + dbName + "/");
-        exportDir.mkdirs();
+        boolean success = exportDir.mkdirs();
+        if (!success) {
+            logger.error("Could not create export directory");
+            return;
+        }
 
         String fileName = getExportFileName(language);
         File file = new File(exportDir, fileName + ".zip");
@@ -96,6 +100,6 @@ public class ScheduledDumpService {
     }
 
     private String getExportFileName(Language language) {
-        return  "pledarigrond_export_json_" + DbSelector.getDbNameByLanguage(pgEnvironment, language);
+        return "pledarigrond_export_json_" + DbSelector.getDbNameByLanguage(pgEnvironment, language);
     }
 }
