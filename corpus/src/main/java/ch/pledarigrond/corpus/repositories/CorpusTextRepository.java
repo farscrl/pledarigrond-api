ad791edf74355566dd0ea462aa8e1fb8b167fa6e
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-08-21 14:10:28 +0200

    feat: make corpus search case insensitive

--- corpus/src/main/java/ch/pledarigrond/corpus/repositories/CorpusTextRepository.java
+++ corpus/src/main/java/ch/pledarigrond/corpus/repositories/CorpusTextRepository.java
@@ -3,11 +3,14 @@ package ch.pledarigrond.corpus.repositories;
 import ch.pledarigrond.corpus.entities.CorpusText;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CorpusTextRepository extends MongoRepository<CorpusText, String> {
+
+    @Query("{ 'lang': ?0, 'text': { $regex: ?1, $options: 'i' } }")
     List<CorpusText> findByLangAndTextContaining(String lang, String text, Pageable pageable);
 }
