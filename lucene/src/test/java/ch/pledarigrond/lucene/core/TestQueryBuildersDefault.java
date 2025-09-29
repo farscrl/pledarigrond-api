75c3f4718d060d73ce9bb65c045614271dc35c07
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-09-29 09:05:10 +0200

    chore: update lucene

--- lucene/src/test/java/ch/pledarigrond/lucene/core/TestQueryBuildersDefault.java
+++ lucene/src/test/java/ch/pledarigrond/lucene/core/TestQueryBuildersDefault.java
@@ -18,7 +18,7 @@ public class TestQueryBuildersDefault extends TestQueryBuildersBase {
 	public void testDefaultQueryGerman() throws InvalidQueryException, NoIndexAvailableException, BrokenIndexException {
 		Page<EntryVersionDto> result = luceneIndexManager.query(IndexTestHelpers.getSearchCriteria(SearchDirection.GERMAN, SearchMethod.NORMAL, "haus"), IndexTestHelpers.getPagination());
 		String[] found = getStrings(FieldName.DESTICHWORT, result);
-		String[] expected = new String[] {"Haus", "Haus", "Haus", "Haus", "Das Haus brennt", "Kranken(haus)", "hausen", "Haushalt"};
+		String[] expected = new String[] {"Haus", "Haus", "Haus", "Haus", "Kranken(haus)", "Das Haus brennt", "hausen", "Haushalt"};
 		printArray(found);
 		assertArrayEquals(expected, found);
 	}
@@ -27,7 +27,7 @@ public class TestQueryBuildersDefault extends TestQueryBuildersBase {
 	public void testDefaultQueryRomansh() throws InvalidQueryException, NoIndexAvailableException, BrokenIndexException {
 		Page<EntryVersionDto> result = luceneIndexManager.query(IndexTestHelpers.getSearchCriteria(SearchDirection.ROMANSH, SearchMethod.NORMAL, "chasa"), IndexTestHelpers.getPagination());
 		String[] found = getStrings(FieldName.RMSTICHWORT, result);
-		String[] expected = new String[] { "chasa", "chasa", "chasa", "a chasa", "a chasa", "a chasa", "a chasa", "a chasa", "anavos a chasa", "encunter chasa", "fatg en chasa", "La chasa arda", "Ve a chasa!", "chasan" };
+		String[] expected = new String[] { "chasa", "chasa", "chasa", "a chasa", "a chasa", "a chasa", "a chasa", "a chasa", "encunter chasa", "anavos a chasa", "fatg en chasa", "La chasa arda", "Ve a chasa!", "chasan" };
 		printArray(found);
 		assertArrayEquals(expected, found);
 	}
@@ -36,14 +36,14 @@ public class TestQueryBuildersDefault extends TestQueryBuildersBase {
 	public void testDefaultQueryBoth() throws InvalidQueryException, NoIndexAvailableException, BrokenIndexException {
 		Page<EntryVersionDto> result = luceneIndexManager.query(IndexTestHelpers.getSearchCriteria(SearchDirection.BOTH, SearchMethod.NORMAL, "chasa"), IndexTestHelpers.getPagination());
 		String[] found = getStrings(FieldName.RMSTICHWORT, result);
-		String[] expected = new String[] { "chasa", "chasa", "chasa", "a chasa", "a chasa", "La chasa arda", "encunter chasa", "Ve a chasa!", "a chasa", "fatg en chasa", "a chasa", "a chasa", "anavos a chasa", "chasan" };
+		String[] expected = new String[] { "chasa", "chasa", "chasa", "a chasa", "a chasa", "encunter chasa", "a chasa", "a chasa", "a chasa", "La chasa arda", "Ve a chasa!", "fatg en chasa", "anavos a chasa", "chasan" };
 		printArray(found);
 		assertArrayEquals(expected, found);
 
 		result = luceneIndexManager.query(IndexTestHelpers.getSearchCriteria(SearchDirection.BOTH, SearchMethod.NORMAL, "haus"), IndexTestHelpers.getPagination());
 		found = getStrings(FieldName.DESTICHWORT, result);
 
-		expected = new String[] {"Haus", "Haus", "Haus", "Haus", "Das Haus brennt", "Kranken(haus)", "hausen", "Haushalt"};
+		expected = new String[] {"Haus", "Haus", "Haus", "Haus", "Kranken(haus)", "Das Haus brennt", "hausen", "Haushalt"};
 		printArray(found);
 		assertArrayEquals(expected, found);
 	}
